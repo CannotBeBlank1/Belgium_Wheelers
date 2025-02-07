d0b1d22ce8f9666dedc6b07ed2ffd1255a85a434
CannotBeBlank1 <bruno.bovy@gmail.com>
2025-02-07 22:40:05 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -25,4 +25,34 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     setInterval(changeBannerImage, 3000); // Changer l'image toutes les 3 secondes
+
+    var languageSelector = document.getElementById('language-selector');
+    if (languageSelector) {
+        languageSelector.addEventListener('change', function() {
+            var selectedLanguage = languageSelector.value;
+            loadLanguageFile(selectedLanguage);
+        });
+    }
+    
+    function loadLanguageFile(language) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'locales/' + language + '.json', true);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                var translations = JSON.parse(xhr.responseText);
+                applyTranslations(translations);
+            }
+        };
+        xhr.send();
+    }
+    
+    function applyTranslations(translations) {
+        document.querySelectorAll('[data-translate-key]').forEach(function(element) {
+            var key = element.getAttribute('data-translate-key');
+            if (translations[key]) {
+                element.textContent = translations[key];
+            }
+        });
+    }
+
 });
